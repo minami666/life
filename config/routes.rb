234b19808cd6497d30d759c1666647c668a4a859
中里南 <minami.rage@gmail.com>
2020-02-02 13:02:54 +0900

    add : header

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  root "diaries#index"
+
   devise_for :users,
     controllers: { registrations: 'registrations' }
-  root "diaries#index"
   resources :users
   resources :diaries
 end
