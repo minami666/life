3802c1f832764006afd1108c8d4891b2ac4f9e48
中里南 <minami.rage@gmail.com>
2020-02-02 14:09:06 +0900

    add : 全ページ共通要素

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,23 +1,50 @@
 class DiariesController < ApplicationController
+  before_action :set_diary, only: [:show,:edit,:update,:destroy]
 
   def index
+    @diaries = Diary.all
   end
 
   def show
   end
 
   def new
+    @diary = Diary.new
   end
 
   def create
+    @diary = Diary.create(diary_params)
+    if @diary.save
+      redirect_to root_path
+    else
+      render "new"
+    end
   end
 
   def edit
   end
 
   def update
+    if @diary.update
+      redirect_to root_path
+    else
+      render "edit"
+    end
+
   end
 
   def destroy
+    @diary.destroy
+    redirect_to root_path
   end
+
+  private
+    def diary_params
+      params.require(:diary).permit(:title,:text,:image)
+    end
+
+    def set_diary
+      @diary = Diary.find(params[:id])
+    end
+
 end
