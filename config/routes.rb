bc7d3dca521108417b4b72e369a58f41f54fc5c2
中里南 <minami.rage@gmail.com>
2020-02-02 13:37:08 +0900

    fix : deviseログアウト後のpath変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
 
   devise_for :users,
     controllers: { registrations: 'registrations' }
+
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+
   resources :users
   resources :diaries
 end
